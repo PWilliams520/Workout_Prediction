5f5e53be6642d3551c58e49bf3daf073b7b1aa59
Esteban Murillo <stbamb@gmail.com>
2019-11-13 22:04:55 -0500

    First attempt at Moving average

--- main.py
+++ main.py
@@ -6,6 +6,7 @@
 # python_version: 3.7
 # notes         : Data Mining Project
 # ==============================================================================
+import config
 import utils
 
 
@@ -15,13 +16,30 @@ def main():
     del activities[0]
 
     activities = utils.cleanUpData(activities)
-    clean_up_data = utils.dropAllLines(header, activities)
-    clean_up_headers = clean_up_data[0]
-    clean_up_activities = clean_up_data[1]
+    cleaned_up_data = utils.dropAllLines(header, activities)
+    cleaned_up_headers = cleaned_up_data[0]
+    cleaned_up_activities = cleaned_up_data[1]
 
-    print(clean_up_headers)
-    print(clean_up_activities)
-    print(len(clean_up_activities))
+    print(cleaned_up_headers)
+    print(cleaned_up_activities)
+    print(len(cleaned_up_activities))
+
+    all_hr = [int(x[1]) for x in cleaned_up_activities]
+    print(all_hr)
+
+    moving_avg_hr = calculateMovingAverage(config.MOVING_AVG_WINDOW, all_hr)
+    print("moving_avg_hr:\n", moving_avg_hr)
+    print(len(moving_avg_hr))
+
+
+def calculateMovingAverage(window, cleaned_up_activities):
+    moving_avg = []
+    for i in range(len(cleaned_up_activities)):
+        if i < window:
+            moving_avg.append(-1)
+        else:
+            moving_avg.append(sum([x for x in cleaned_up_activities[i - window:i]]) / window)
+    return moving_avg
 
 
 if __name__ == "__main__":

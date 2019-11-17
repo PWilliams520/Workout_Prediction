a060c517aec15c8d35e60aa46ec5596a44d364bc
Esteban Murillo <stbamb@gmail.com>
2019-11-16 23:32:59 -0500

    Organizing code

--- main.py
+++ main.py
@@ -29,28 +29,23 @@ def main():
     all_hr = [int(x[1]) for x in cleaned_up_activities]
     all_hr_labels = [0 if value <= config.HR_THRESHOLD else 1 for value in all_hr]
 
-    print(all_hr)
-    print(all_hr_labels)
+    print("HR values (all time):\n", all_hr)
+    print("\nHR labels (all time):\n", all_hr_labels)
 
     best_window = calculateBestWindow(all_hr)
-    print(best_window)
+    print("\nBest window value found for distances: {}".format(best_window))
 
     simple_moving_average = calculateSimpleMovingAverage(best_window, all_hr)
     simple_moving_average_labels = [0 if value <= config.HR_THRESHOLD else 1 for value in simple_moving_average]
 
-    #print(simple_moving_average)
-    print(simple_moving_average_labels)
-
+    # print(simple_moving_average)
+    # print("Predicted values for window = {}:\n{}".format(best_window, simple_moving_average_labels))
+    # print("Actual values for window = {}:\n{}".format(best_window, all_hr_labels[best_window:]))
     value = getPredictedAccuracyLabels(all_hr_labels, simple_moving_average_labels, best_window)
-    print(value)
+    print("For window {}, getting accuracy of {:.2%}\n".format(best_window, value))
 
     values = calculateBestWindow2(all_hr)
-
-    with open('a.csv', 'w') as f:
-        for i in range(0, len(values)):
-            f.write(str(values[i][0]) + ', ' + str(values[i][1]))
-            f.write('\n')
-
+    print(values)
 
 
 def calculateBestWindow(all_hr):
@@ -59,13 +54,7 @@ def calculateBestWindow(all_hr):
         simple_avg_hr = calculateSimpleMovingAverage(window, all_hr)
         moving_hr_accuracy = getAccuracy(all_hr, simple_avg_hr, window)
         simple_moving_avg_accuracy.append((moving_hr_accuracy, window))
-
-        # cumulative_avg_hr = calculateCumulativeMovingAverage(window, all_hr)
-        # cumulative_hr_accuracy = getAccuracy(all_hr, cumulative_avg_hr, window)
-        # c_acc.append(cumulative_hr_accuracy)
-
     simple_moving_avg_accuracy = utils.mergesort(simple_moving_avg_accuracy)
-
     return simple_moving_avg_accuracy[0][1]
 
 
@@ -73,22 +62,11 @@ def calculateBestWindow2(all_hr):
     simple_moving_avg_accuracy = []
     for window in range(1, len(all_hr)):
         simple_avg_hr = calculateSimpleMovingAverage(window, all_hr)
-
         all_hr_labels = [0 if value <= config.HR_THRESHOLD else 1 for value in all_hr]
         simple_moving_average_labels = [0 if value <= config.HR_THRESHOLD else 1 for value in simple_avg_hr]
-
         moving_hr_accuracy = getPredictedAccuracyLabels(all_hr_labels, simple_moving_average_labels, window)
         simple_moving_avg_accuracy.append((moving_hr_accuracy, window))
-
-        # cumulative_avg_hr = calculateCumulativeMovingAverage(window, all_hr)
-        # cumulative_hr_accuracy = getAccuracy(all_hr, cumulative_avg_hr, window)
-        # c_acc.append(cumulative_hr_accuracy)
-
-    simple_moving_avg_accuracy = utils.mergesort(simple_moving_avg_accuracy)
-
-    print(simple_moving_avg_accuracy)
-
-    return simple_moving_avg_accuracy
+    return utils.mergesort(simple_moving_avg_accuracy)
 
 
 def getAccuracy(original_set, predicted_set, window):
